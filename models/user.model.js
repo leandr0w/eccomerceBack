b3091097e878593bcc5a6a50437b52056ba531cb
leandro wasinger <leowasingerz@gmail.com>
2023-02-18 21:36:06 -0300

    definiendo tablas db

---
+++ models/user.model.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require("sequelize");
+const { db } = require("../database/db");
+
+//Importante colocar siempre con la primer letra mayuscula porque es un modelo
+const User = db.define('user', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: true,
+        type: DataTypes.INTEGER
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    surname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false 
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'user',
+        enum: ['user', 'admin']
+    },
+    status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+    }
+})
+
+module.exports = User
